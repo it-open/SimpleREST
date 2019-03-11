b0f400056d209b88459e77618a0eb0826c7c986a
Roland Schuller <roland@it-open.at>
2019-03-11 11:05:33 +0100

    SHA-512

--- src/main/java/at/itopen/simplerest/microservice/loadbalancer/Encryption.java
+++ src/main/java/at/itopen/simplerest/microservice/loadbalancer/Encryption.java
@@ -6,8 +6,10 @@
 package at.itopen.simplerest.microservice.loadbalancer;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -92,6 +94,35 @@ public class Encryption {
 
     public static String correctINITV(String key) {
         return pad(key, 16, "-");
+    }
+
+    public static String SHA512(String input) {
+        try {
+            // getInstance() method is called with algorithm SHA-512 
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+            // digest() method is called 
+            // to calculate message digest of the input string 
+            // returned as array of byte 
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation 
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value 
+            String hashtext = no.toString(16);
+
+            // Add preceding 0s to make it 32 bit 
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            // return the HashText 
+            return hashtext;
+        } // For specifying wrong message digest algorithms 
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
