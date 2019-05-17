0440109280dd92f3f87f7d770086e81b15406d02
Roland Schuller <roland@it-open.at>
2019-05-17 13:49:49 +0200

    GZip Compression

--- src/main/java/at/itopen/simplerest/RestHttpServerInitializer.java
+++ src/main/java/at/itopen/simplerest/RestHttpServerInitializer.java
@@ -8,6 +8,7 @@ package at.itopen.simplerest;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpContentCompressor;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
 
@@ -16,7 +17,7 @@ import io.netty.handler.codec.http.HttpResponseEncoder;
  * @author roland
  */
 public class RestHttpServerInitializer extends ChannelInitializer<SocketChannel> {
-    
+
     RestHttpServer webserver;
 
     /**
@@ -26,21 +27,21 @@ public class RestHttpServerInitializer extends ChannelInitializer<SocketChannel>
     public RestHttpServerInitializer(RestHttpServer webserver) {
         this.webserver = webserver;
     }
-    
-    
+
     /**
      *
      * @param socketChannel
      * @throws Exception
      */
     @Override
-	protected void initChannel(SocketChannel socketChannel) throws Exception {
-		ChannelPipeline p = socketChannel.pipeline();
-		p.addLast("http.decoder", new HttpRequestDecoder());
-		//p.addLast("");
-		p.addLast("http.encoder", new HttpResponseEncoder());
-		//p.addLast("http.deflater", new HttpResponseEncoder());
-		p.addLast("http.request-handler", new RestHttpRequestDispatchHandler(webserver));
-	}
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline p = socketChannel.pipeline();
+        p.addLast("http.decoder", new HttpRequestDecoder());
+        //p.addLast("");
+        p.addLast("http.encoder", new HttpResponseEncoder());
+        p.addLast("deflater", new HttpContentCompressor(1));
+        //p.addLast("http.deflater", new HttpResponseEncoder());
+        p.addLast("http.request-handler", new RestHttpRequestDispatchHandler(webserver));
+    }
 
 }
